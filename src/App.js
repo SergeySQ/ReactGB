5841ce48dc7ca08a23b01a9250515b3b390c6d6b
Sergey Sudare <sergey.sudarev2016@yandex.kz>
2021-10-29 22:33:27 +0600

    My awesome chat app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let name = prompt("Как ваше имя?");
 
+const App = (props) => {
+	const [messageList, setMessageList] = useState([]);
+	const [value, setValue] = useState("");
+
+	const saveMessage = (event) => {
+		setValue(event.target.value);
+	};
+
+	const addMessage = (author, text) => {
+		const newMessageList = [...messageList];
+		const newMessage = {
+			author,
+			text,
+		};
+		newMessageList.push(newMessage);
+		setMessageList(newMessageList);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		addMessage(name, value);
+		setValue("");
+	};
+
+	return (
+		<div className={styles.chatBox}>
+			<h2 className={styles.title}>{props.context}</h2>
+			<div className={styles.chat}>
+				<ul className={styles.ulList}>
+					{messageList.map((item) => (
+						<li className={styles.chatMessage}>
+							<span className={styles.author}>{item.author}</span>
+							: {item.text}
+						</li>
+					))}
+				</ul>
+			</div>
+			<form onSubmit={onSubmit} className={styles.form}>
+				<input
+					value={value}
+					onChange={saveMessage}
+					className={styles.input}
+					type="text"
+					placeholder="Введите сообщение"
+				/>
+				<button className={styles.button} type="submit">
+					Отправить
+				</button>
+			</form>
+		</div>
+	);
+};
 export default App;
---
+++ src/Components/Mess.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Welcome(props) {
+	return <h1>Привет, {props.name}</h1>;
+}
+export default Welcome;
