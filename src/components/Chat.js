67ef49636865da940fa9e766d525229abe718d95
Sergey Sudare <sergey.sudarev2016@yandex.kz>
2021-11-09 21:01:28 +0600

    Работа  react-router-dom

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,14 @@
 import { ListItem, ListItemText } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import propTypes from "prop-types";
 
-export const Chat = ({ name }) => {
+export const Chat = ({ name, id }) => {
 	return (
-		<ListItem>
+		<ListItem component={Link} to={`/chats/${id}`}>
 			<ListItemText>{name}</ListItemText>
 		</ListItem>
 	);
 };
-
 Chat.propTypes = {
 	id: propTypes.string,
 	name: propTypes.string,
---
+++ src/routes/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+export const HelloChat = () => {
+	return (
+		<div>
+			<h1>Так… что у нас здесь?</h1>
+		</div>
+	);
+};
